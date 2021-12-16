4f45bd1f8665a5b6a4a31fb928d5478073d2e5ea
phu <phamxuanphu98@gmail.com>
2021-12-16 08:27:03 +0700

    commit

--- PhuLongCRM/Models/ViewData.cs
+++ PhuLongCRM/Models/ViewData.cs
@@ -16,17 +16,17 @@ namespace PhuLongCRM.Models
         {
             return new List<OptionSetFilter>() {
                 new OptionSetFilter(){ Val="100000000",Label="Thành phố"},
-                new OptionSetFilter(){Val="100000001", Label="Hồ bơi" },
+                new OptionSetFilter(){Val="100000001", Label="Bể bơi" },  // hồ bơi
                 new OptionSetFilter(){ Val="100000002",Label="Công viên"},
                 new OptionSetFilter(){ Val="100000003",Label="Mặt tiền"},
-                new OptionSetFilter(){ Val="100000004",Label="Garden"},
-                new OptionSetFilter(){ Val="100000006",Label="High Way"},
-                new OptionSetFilter(){ Val="100000007",Label="Lake"},
-                new OptionSetFilter(){ Val="100000008",Label="River"},
-                new OptionSetFilter(){ Val="100000009",Label="Sea"},
-                new OptionSetFilter(){ Val="100000010",Label="01 Side Open"},
-                new OptionSetFilter(){ Val="100000011",Label="02 Side Open"},
-                new OptionSetFilter(){ Val="100000012",Label="Pool"},
+                new OptionSetFilter(){ Val="100000004",Label="Sân vườn"},
+                new OptionSetFilter(){ Val="100000006",Label="Xa lộ"},
+                new OptionSetFilter(){ Val="100000007",Label="Hồ"}, //lake
+                new OptionSetFilter(){ Val="100000008",Label="Sông"},
+                new OptionSetFilter(){ Val="100000009",Label="Biển"},
+                new OptionSetFilter(){ Val="100000010",Label="01 mặt thoáng"}, 
+                new OptionSetFilter(){ Val="100000011",Label="02 mặt thoáng"},
+                new OptionSetFilter(){ Val="100000012",Label="Hồ bơi"}, // pool
 
             };
         }
