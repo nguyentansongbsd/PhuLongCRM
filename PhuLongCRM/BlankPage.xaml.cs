bb6ed5bfee247cabe5626c6e0c1a1a3d46098a99
nguyentansongbsd <songnt@bsdinsight.com>
2021-12-24 09:32:32 +0700

    song tao control phone

---
+++ PhuLongCRM/BlankPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PhuLongCRM
+{
+    public partial class BlankPage : ContentPage
+    {
+        public BlankPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PhuLongCRM/Controls/PhoneEntryControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PhuLongCRM.Controls
+{
+    public partial class PhoneEntryControl : MainEntry
+    {
+        public PhoneEntryControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
