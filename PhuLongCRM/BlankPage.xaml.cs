048214aa92f0bb03bfff8a323ca40be8fd48f078
phu <phamxuanphu98@gmail.com>
2022-06-02 18:04:41 +0700

    fix loi nho

--- PhuLongCRM/BlankPage.xaml.cs
+++ PhuLongCRM/BlankPage.xaml.cs
@@ -13,52 +13,9 @@ namespace PhuLongCRM
 {
     public partial class BlankPage : ContentPage
     {
-        //private string _num;
-        //public string num { get => _num; set { _num = value; OnPropertyChanged(nameof(num)); } }
-        public static string tessst { get; set; }
         public BlankPage()
         {
             InitializeComponent();
-            //this.BindingContext = this;
-           // test.InputValue = num = "840336021479";
-            //zxing.OnScanResult += (result) =>
-            //Device.BeginInvokeOnMainThread(() =>
-            //{
-            //    lblResult.Text = result.Text;
-            //});
-
-
-        }
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            //zxing.IsScanning = true;
-        }
-
-        protected override void OnDisappearing()
-        {
-            //zxing.IsScanning = false;
-
-            base.OnDisappearing();
-        }
-
-        public static async Task<GetTokenResponse> getSharePointToken()
-        {
-            var client = BsdHttpClient.Instance();
-            var request = new HttpRequestMessage(HttpMethod.Post, "https://login.microsoftonline.com/b8ff1d2e-28ba-44e6-bf5b-c96188196711/oauth2/token");//" https://login.microsoftonline.com/b8ff1d2e-28ba-44e6-bf5b-c96188196711/oauth2/token"
-            var formContent = new FormUrlEncodedContent(new[]
-                {
-                        new KeyValuePair<string, string>("client_id", "bbdc1207-6048-415a-a21c-02a734872571"),
-                        new KeyValuePair<string, string>("client_secret", "_~~NDM9PVbrSD22Ef-.qRnxioPHcG5xsJ8"),
-                        new KeyValuePair<string, string>("grant_type", "client_credentials"),
-                        new KeyValuePair<string, string>("resource", OrgConfig.Resource)
-                    });
-            request.Content = formContent;
-            var response = await client.SendAsync(request);
-            var body = await response.Content.ReadAsStringAsync();
-            GetTokenResponse tokenData = JsonConvert.DeserializeObject<GetTokenResponse>(body);
-            return tokenData;
         }
     }
 }
--- PhuLongCRM/Controls/CenterPopup.xaml.cs
+++ PhuLongCRM/Controls/CenterPopup.xaml.cs
@@ -10,12 +10,51 @@ using Xamarin.Forms.Xaml;
 namespace PhuLongCRM.Controls
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CenterPopup : StackLayout
+    public partial class CenterPopup : ContentView
     {
-        public View Content { get; set; }
+        public static readonly BindableProperty BodyProperty = BindableProperty.Create(nameof(Body), typeof(View), typeof(CenterPopup), null, propertyChanged: ContentChanged);
+        private static void ContentChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (newValue == null) return;
+            CenterPopup control = (CenterPopup)bindable;
+            control.CreateBody();
+        }
+        public View Body
+        {
+            get { return (View)GetValue(BodyProperty); }
+            set { SetValue(BodyProperty, value); }
+        }
+
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title), typeof(string), typeof(CenterPopup), null, BindingMode.TwoWay);
+        public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
+
+        public event EventHandler Show;
         public CenterPopup()
         {
             InitializeComponent();
+            this.title.SetBinding(Entry.TextProperty, "Title");
+        }
+
+        private void CreateBody()
+        {
+            if (Body != null)
+            {
+                grid_body.Children.Add(Body);
+                Grid.SetColumn(Body, 0);
+                Grid.SetRow(Body, 1);
+            }
+        }
+
+        public void ShowCenterPopup()
+        {
+            EventHandler eventHandler = Show;
+            eventHandler?.Invoke((object)this, EventArgs.Empty);
+            this.IsVisible = true;
+        }
+
+        private void CloseContent_Tapped(object sender, EventArgs e)
+        {
+            this.IsVisible = false;
         }
     }
 }
