fd20fbc576e1990d59d2a1b97315861001f4ba8e
phu <phamxuanphu98@gmail.com>
2022-08-10 17:25:50 +0700

    cmnd

--- PhuLongCRM.Android/Services/PDFSaveAndOpen.cs
+++ PhuLongCRM.Android/Services/PDFSaveAndOpen.cs
@@ -29,55 +29,41 @@ namespace PhuLongCRM.Droid.Services
                 ActivityCompat.RequestPermissions((Activity)Forms.Context, new String[] { Manifest.Permission.WriteExternalStorage }, 1);
             }
 
-            if (Android.OS.Environment.IsExternalStorageEmulated)
+            string filePath = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, Android.OS.Environment.DirectoryDownloads);
+            string fullName = Path.Combine(filePath, fileName);
+
+            // If the file exist on device delete it
+            if (System.IO.File.Exists(fullName))
             {
-                root = Android.OS.Environment.ExternalStorageDirectory.ToString();
-            }
-            else
-                root = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
-
-            Java.IO.File myDir = new Java.IO.File(root + "/PDFFiles");
-            myDir.Mkdir();
-
-            Java.IO.File file = new Java.IO.File(myDir, fileName);
-
-            if (file.Exists()) file.Delete();
-
-            try
-            {
-                FileOutputStream outs = new FileOutputStream(file);
-                outs.Write(stream.ToArray());
-
-                outs.Flush();
-                outs.Close();
-            }
-            catch (Exception e)
-            {
-                exception = e.ToString();
+                // Note: In the second run of this method, the file exists
+                System.IO.File.Delete(fullName);
             }
 
-            if (file.Exists() && contentType != "application/html")
-            {
-                string extension = MimeTypeMap.GetFileExtensionFromUrl(Android.Net.Uri.FromFile(file).ToString());
-                string mimeType = MimeTypeMap.Singleton.GetMimeTypeFromExtension(extension);
-                Intent intent = new Intent(Intent.ActionView);
-                intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.NewTask);
-                Android.Net.Uri path = FileProvider.GetUriForFile(Forms.Context, Android.App.Application.Context.PackageName + ".provider", file);
-                intent.SetDataAndType(path, mimeType);
-                intent.AddFlags(ActivityFlags.GrantReadUriPermission);
+            // Write bytes on "My documents"
+            System.IO.File.WriteAllBytes(fullName, stream.ToArray());
 
-                switch (context)
-                {
-                    case PDFOpenContext.InApp:
-                        Forms.Context.StartActivity(intent);
-                        break;
-                    case PDFOpenContext.ChooseApp:
-                        Forms.Context.StartActivity(Intent.CreateChooser(intent, "Choose App"));
-                        break;
-                    default:
-                        break;
-                }
-            }
+            //if (file.Exists() && contentType != "application/html")
+            //{
+            //    string extension = MimeTypeMap.GetFileExtensionFromUrl(Android.Net.Uri.FromFile(file).ToString());
+            //    string mimeType = MimeTypeMap.Singleton.GetMimeTypeFromExtension(extension);
+            //    Intent intent = new Intent(Intent.ActionView);
+            //    intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.NewTask);
+            //    Android.Net.Uri path = FileProvider.GetUriForFile(Forms.Context, Android.App.Application.Context.PackageName + ".provider", file);
+            //    intent.SetDataAndType(path, mimeType);
+            //    intent.AddFlags(ActivityFlags.GrantReadUriPermission);
+
+            //    switch (context)
+            //    {
+            //        case PDFOpenContext.InApp:
+            //            Forms.Context.StartActivity(intent);
+            //            break;
+            //        case PDFOpenContext.ChooseApp:
+            //            Forms.Context.StartActivity(Intent.CreateChooser(intent, "Choose App"));
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //}
         }
     }
 }
--- PhuLongCRM/Models/ListInforUnitTLKD.cs
+++ PhuLongCRM/Models/ListInforUnitTLKD.cs
@@ -13,45 +13,15 @@ namespace PhuLongCRM.Models
         public string bsd_projectname { get; set; }
         public string description { get; set; }
         public string bsd_unitscodesams { get; set; }
-        public int statuscode { get; set; }
-        public string statuscodevalue
-        {
-            get
-            {
-                switch (statuscode)
-                {
-                    case 1:
-                        return Language.chuan_bi;
-                    case 100000000:
-                        return Language.san_sang;//"Available";
-                    case 100000004:
-                        return Language.giu_cho;//"Queuing";
-                    case 100000007:
-                        return Language.dat_cho;//"Giữ chỗ";
-                    case 100000006:
-                        return Language.dat_coc;//"Reserve";
-                    case 100000005:
-                        return Language.dong_y_chuyen_coc;//"Collected";
-                    case 100000003:
-                        return Language.da_du_tien_coc;//"Deposited";
-                    case 100000001:
-                        return Language.thanh_toan_dot_1;//"1st Installment";
-                    case 100000008:
-                        return Language.du_dieu_dien;//"Đủ điều kiện";
-                    case 100000009:
-                        return Language.da_ky_ttdc_hddc;//"Thỏa thuận đặt cọc";
-                    case 100000002:
-                        return Language.da_ban;//"Sold";
-                    default:
-                        return "";
-                }
-            }
-        }
+        public string statuscode { get; set; }
+        public string statuscode_format { get { return statuscode != null ? StatusCodeUnit.GetStatusCodeById(statuscode.ToString())?.Name : null; } }
+        public string statuscode_color { get { return statuscode != null ? StatusCodeUnit.GetStatusCodeById(statuscode.ToString())?.Background : null; } }
         public DateTime createdon { get; set; }
         public string createdonformat
         {
             get => StringHelper.DateFormat(createdon);
         }
+        public string productnumber { get; set; }
     }
 }
 
