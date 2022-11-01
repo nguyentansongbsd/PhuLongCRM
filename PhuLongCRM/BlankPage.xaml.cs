8ac04a11e1b3e81caf73fc67cf01ede4480c3a54
phu <phamxuanphu98@gmail.com>
2022-11-01 10:26:24 +0700

    pull

--- PhuLongCRM/BlankPage.xaml.cs
+++ PhuLongCRM/BlankPage.xaml.cs
@@ -37,34 +37,34 @@ namespace PhuLongCRM
 
         private async void Init()
         {
-            try
-            {
-                Firebase.Database.
+            //try
+            //{
+            //    Firebase.Database.
                 
-                var options = FirebaseOptions.Builder()
-                            .SetApplicationId("1:998946335721:android:94837bae80edd709fcb978")
-                            .SetApiKey("AIzaSyC1p-TwiVz5Tb2Cj8zCN3iy4O-8qfeV1Kg")
-                            .SetDatabaseUrl("https://sundihome-bsd.firebaseio.com")
-                            .Build();
+            //    var options = FirebaseOptions.Builder()
+            //                .SetApplicationId("1:998946335721:android:94837bae80edd709fcb978")
+            //                .SetApiKey("AIzaSyC1p-TwiVz5Tb2Cj8zCN3iy4O-8qfeV1Kg")
+            //                .SetDatabaseUrl("https://sundihome-bsd.firebaseio.com")
+            //                .Build();
 
 
-                var collection = firebaseClient
-                        .Child("test").Child("DirectSaleData")
-                        .
-                        .Subscribe(async (dbevent) =>
-                        {
+            //    var collection = firebaseClient
+            //            .Child("test").Child("DirectSaleData")
+            //            .
+            //            .Subscribe(async (dbevent) =>
+            //            {
 
-                        });
-            }
-            catch (Exception ex)
-            {
+            //            });
+            //}
+            //catch (Exception ex)
+            //{
 
-            }
+            //}
 
         }
 
 
-        private void AbsoluteLayout_Tapped(object sender, MR.Gestures.TapEventArgs e)
+        private async void AbsoluteLayout_Tapped(object sender, MR.Gestures.TapEventArgs e)
         {
             string id = "08E25682-23E9-41EB-A1B7-6B166562F8AE";
             GetTokenResponse getTokenResponse = await LoginHelper.getSharePointToken();
